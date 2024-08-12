6bd27d4acd1f3efa5bd5de6bcca55ac04cf81f36
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-12 17:07:33 +0200

    create room card component

--- src/app/rooms/rooms.module.ts
+++ src/app/rooms/rooms.module.ts
@@ -7,9 +7,15 @@ import { RoomsRoutingModule } from './rooms-routing.module';
 import { RoomsComponent } from './rooms.component';
 import { RoomListComponent } from './components/room-list/room-list.component';
 import { RoomPanelFilterComponent } from './components/room-panel-filter/room-panel-filter.component';
+import { RoomCardComponent } from './components/room-card/room-card.component';
 
 @NgModule({
-  declarations: [RoomsComponent, RoomListComponent, RoomPanelFilterComponent],
+  declarations: [
+    RoomsComponent,
+    RoomListComponent,
+    RoomPanelFilterComponent,
+    RoomCardComponent,
+  ],
   imports: [CommonModule, RouterModule, RoomsRoutingModule],
 })
 export class RoomsModule {}
