6fe77f8574af8858b6df2c11685a11a0514e8f1b
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-13 18:05:06 +0200

    test for RoomPanelFilterComponent

--- src/app/rooms/components/room-panel-filter/room-panel-filter.component.spec.ts
+++ src/app/rooms/components/room-panel-filter/room-panel-filter.component.spec.ts
@@ -4,6 +4,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 // Project imports
 import { RoomPanelFilterComponent } from './room-panel-filter.component';
+import { IParamsFilters } from '../../../shared/interfaces';
+import { clickElement } from '../../../../testing';
 
 describe('RoomPanelFilterComponent', () => {
   let component: RoomPanelFilterComponent;
@@ -22,5 +24,50 @@ describe('RoomPanelFilterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should send the form successfully onSubmit', () => {
+    const mockForm = {
+      name: 'Test',
+      capacity: 16,
+      occupancy: 7,
+      floor1: true,
+    };
+
+    const mockFilters: IParamsFilters = {
+      name: 'Test',
+      capacity: 16,
+      occupancy: 7,
+      floor: [1],
+    };
+
+    spyOn(component.findRoomsEmit, 'emit');
+
+    component.form.patchValue(mockForm);
+    fixture.detectChanges();
+
+    component.onSubmit();
+
+    expect(component.findRoomsEmit.emit).toHaveBeenCalledWith(mockFilters);
+  });
+
+  it('should call onReset() when do click button', () => {
+    spyOn(component.findRoomsEmit, 'emit');
+
+    clickElement(fixture, 'btn-reset', true);
+
+    fixture.detectChanges();
+
+    expect(component.findRoomsEmit.emit).toHaveBeenCalledWith({ name: '' });
+  });
+
+  it('should call onClose() when do click button', () => {
+    spyOn(component, 'onClose');
+
+    clickElement(fixture, 'btn-close', true);
+
+    fixture.detectChanges();
+
+    expect(component.onClose).toHaveBeenCalled();
   });
 });
