03c0b1a0f80f1bf168a80869401ff0677198e041
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-13 14:09:23 +0200

    add helpers for testing

---
+++ src/testing/index.ts
@@ -0,0 +1,7 @@
+export * from './async-data';
+export * from './finders';
+export * from './click';
+export * from './forms';
+export * from './activated-route-stub';
+export * from './router-link-directive-stub';
+export * from './snapshot';
---
+++ src/testing/router-link-directive-stub.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener, Input } from '@angular/core';
+
+@Directive({
+  standalone: true,
+  selector: '[routerLink]',
+})
+export class RouterLinkDirectiveStub {
+  @Input('routerLink') linkParams: any;
+  navigatedTo: any = null;
+
+  @HostListener('click')
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
