a3ed2d2342206490e96c02253c869c1a38440eaa
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-13 14:06:29 +0200

    test for CapacityOccupancyPipe

--- src/app/shared/pipes/capacityOccupancy/capacity-occupancy.pipe.spec.ts
+++ src/app/shared/pipes/capacityOccupancy/capacity-occupancy.pipe.spec.ts
@@ -1,9 +1,21 @@
 // Angular imports
 import { CapacityOccupancyPipe } from './capacity-occupancy.pipe';
 
-describe('CapacityOccupancyPipe', () => {
+fdescribe('CapacityOccupancyPipe', () => {
   it('create an instance', () => {
     const pipe = new CapacityOccupancyPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return `50% - 5/10` capacity=10 y occupancy=5', () => {
+    const pipe = new CapacityOccupancyPipe();
+    const rta = pipe.transform(10, 5);
+    expect(rta).toEqual('50% - 5/10');
+  });
+
+  it('should return `0% - 0/0` capacity=10 y occupancy=5', () => {
+    const pipe = new CapacityOccupancyPipe();
+    const rta = pipe.transform(0, 5);
+    expect(rta).toEqual('0% - 0/0');
   });
 });
